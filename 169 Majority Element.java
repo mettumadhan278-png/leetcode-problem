2d6805c732a8088e62098cf4ab30dc18cdf1966b
mettumadhan278-png <mettumadhan278@gmail.com>
2026-07-20 10:48:45 +0530

    Implement majority element algorithm

---
+++ 169 Majority Element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+      int canditate = 0;
+      int count = 0;
+
+      for(int num:nums){
+        if(count==0){
+            canditate = num;
+        }
+        if(canditate == num){
+            count--;
+        }
+    }
+    return canditate;
+    }
+}

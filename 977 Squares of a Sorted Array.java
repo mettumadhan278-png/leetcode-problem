32f851872d857369d986b0f1ccb231599702c563
mettumadhan278-png <mettumadhan278@gmail.com>
2026-07-23 09:41:45 +0530

    Refactor sortedSquares method to use sorting

--- 977 Squares of a Sorted Array.java
+++ 977 Squares of a Sorted Array.java
@@ -1,26 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int n = nums.length;
-        int[] result = new int[n];
-        
-        int left = 0;
-        int right = n - 1;
-        int index = n - 1; 
-        
-        while (left <= right) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-            
-            if (leftSquare > rightSquare) {
-                result[index] = leftSquare;
-                left++;
-            } else {
-                result[index] = rightSquare;
-                right--;
-            }
-            index--;
-        }
-        
-        return result;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        } 
+        Arrays.sort(nums);
+        return nums;
     }
 }

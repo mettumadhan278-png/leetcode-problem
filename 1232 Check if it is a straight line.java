1271d1ee88965de0fdee02edfb8be9a9e075be74
mettumadhan278-png <mettumadhan278@gmail.com>
2026-07-23 12:53:46 +0530

    Refactor checkStraightLine method by removing comments
    Removed comments to clean up the code.

--- 1232 Check if it is a straight line.java
+++ 1232 Check if it is a straight line.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
-        // Find initial dx and dy using the first two points
+        
         int x0 = coordinates[0][0], y0 = coordinates[0][1];
         int x1 = coordinates[1][0], y1 = coordinates[1][1];
 
         int dx = x1 - x0;
         int dy = y1 - y0;
 
-        // Check if all other points maintain the same slope ratio
         for (int i = 2; i < coordinates.length; i++) {
             int x = coordinates[i][0];
             int y = coordinates[i][1];
 
-            // Cross-multiplication check: dy * (x - x0) == dx * (y - y0)
             if (dy * (x - x0) != dx * (y - y0)) {
                 return false;
             }
